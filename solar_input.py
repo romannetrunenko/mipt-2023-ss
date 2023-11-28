4bf373c59350c742811dd13fdcefef0fb9eca747
Derxail <kuzmenko.ta@phystech.edu>
2023-11-28 19:23:42 +0300

    saving

--- solar_input.py
+++ solar_input.py
@@ -19,9 +19,13 @@ def read_space_objects_data_from_file(input_filename):
             if len(line.strip()) == 0 or line[0] == '#':
                 continue  # пустые строки и строки-комментарии пропускаем
             object_type = line.split()[0].lower()
-            if object_type == "star":  # FIXME: do the same for planet
+            if object_type == "star":
                 star = Star()
                 parse_star_parameters(line, star)
+                objects.append(star)
+            elif object_type == "planet":
+                planet = Planet()
+                parse_planet_parameters(line, star)
                 objects.append(star)
             else:
                 print("Unknown space object")
@@ -52,6 +56,7 @@ def parse_star_parameters(line, star):
     star.y = attrs[5]
     star.Vx = attrs[6]
     star.Vy = attrs[7]
+    star.type = "star"
 
 def parse_planet_parameters(line, planet):
     """Считывает данные о планете из строки.
@@ -77,6 +82,7 @@ def parse_planet_parameters(line, planet):
     planet.y = attrs[5]
     planet.Vx = attrs[6]
     planet.Vy = attrs[7]
+    planet.type = "planet"
 
 
 def write_space_objects_data_to_file(output_filename, space_objects):
@@ -93,7 +99,8 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
             print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
-            # FIXME: should store real values
+            line = f'{obj.type.capitalize()} {obj.R} {obj.color} {obj.m} {obj.x} {obj.y} {obj.Vx} {obj.Vy}'
+            out_file.write(line + '\n')
 
 # FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
 
